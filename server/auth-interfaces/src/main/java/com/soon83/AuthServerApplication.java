e701760874d7ed37d4cb641765af2e0e4dfa21c8
ds <2601948@gmail.com>
2024-10-09 14:26:43 +0900

    create project

---
+++ server/auth-interfaces/src/main/java/com/soon83/AuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.soon83;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerApplication.class, args);
+    }
+
+}
---
+++ server/main-interfaces/src/test/java/com/soon83/springmodulex/SpringModuleXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soon83.springmodulex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModuleXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
