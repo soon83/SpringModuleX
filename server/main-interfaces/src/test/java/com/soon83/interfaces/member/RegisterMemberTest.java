1cd0c904255bcb6560ffa593db9c757380f7c2a0
ds <2601948@gmail.com>
2024-10-27 03:25:04 +0900

    2. create test case registerMember

--- server/main-interfaces/src/test/java/com/soon83/interfaces/member/RegisterMemberTest.java
+++ server/main-interfaces/src/test/java/com/soon83/interfaces/member/RegisterMemberTest.java
@@ -1,5 +1,12 @@
 package com.soon83.interfaces.member;
 
-class RegisterMemberTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+class RegisterMemberTest {
+    @Test
+    @DisplayName("회원을 등록한다")
+    void registerMember() {
+
+    }
 }
